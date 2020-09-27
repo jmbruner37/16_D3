39cdc1455fed9fa6ddf12ad896e6c730b76a8107
jmbruner37 <jmbruner37@gmail.com>
2020-09-26 20:39:59 -0700

    axes label

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -28,12 +28,88 @@ var chartGroup = svg.append("g")
 
 // Load data from data.csv
 d3.csv("./assets/data/data.csv", function(error, statedata) {  
-    // Log an error if one exists
-    if (error) return console.warn(error)
+// Log an error if one exists
+if (error) return console.warn(error)
 
-    statedata.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.obesity = +data.obesity;
-      });
+statedata.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.obesity = +data.obesity;
+  });
+// Create Scales
+var xScale = d3.scaleLinear()
+.domain([8, d3.max(statedata, d => d.poverty)])
+.range([0, width]);
 
+var yLinearScale = d3.scaleLinear()
+.domain([4, d3.max(statedata, d => d.obesity)])
+.range([height, 0]);
+
+// Create Axes
+
+var bottomAxis = d3.axisBottom(xScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+
+// Append the axes to the chartGroup
+// Add bottomAxis
+chartGroup.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(bottomAxis);
+
+
+// Add leftAxis to the left side of the display
+chartGroup.append("g")
+.call(leftAxis);
+
+// Create Circles
+var circlesGroup = chartGroup.selectAll("circle")
+.data(statedata)
+.enter()
+.append("circle")
+.attr("cx", d => xScale(d.poverty))
+.attr("cy", d => yLinearScale(d.obesity))
+.attr("r", 15)
+.attr("class", function(d) {
+    return "stateCircle " + d.abbr;
+  })
+.attr("fill", "red")
+.attr("opacity", ".7")
+
+var toolTip = d3.tip()
+.attr("class", "tooltip")
+.attr([1, -1])
+.html(function(d) {
+return (`${d.abbr}`);
+});
+
+// Create tooltip in the chart
+
+chartGroup.call(toolTip);
+
+circlesGroup.on("click", function(data) {
+toolTip.show(data, this);
+})
+// onmouseout event
+.on("mouseout", function(data, index) {
+  toolTip.hide(data);
+});
+
+
+
+
+chartGroup.append("text")
+.attr("transform", `translate(${width / 3}, ${height + margin.top + 20})`)
+.text("Percentage of Population in Poverty");
+
+// Create axes labels
+chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 40)
+  .attr("x", 0 - (height/1.2))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .text("Percentage of Population that are Obese");
+
+
+});
 
