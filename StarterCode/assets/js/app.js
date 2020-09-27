48e91b40bd4c6c5133de68c097de6466dfc8cda3
jmbruner37 <jmbruner37@gmail.com>
2020-09-26 20:26:41 -0700

    load data

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -10,3 +10,30 @@ var margin = {
   left: 100
 };
 
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, 
+//and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Load data from data.csv
+d3.csv("./assets/data/data.csv", function(error, statedata) {  
+    // Log an error if one exists
+    if (error) return console.warn(error)
+
+    statedata.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.obesity = +data.obesity;
+      });
+
+
